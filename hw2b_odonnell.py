0f28ae579236fb531ff4c9bcc7a4fe4cfb110bf1
Sydnee ODonnell <odonnells@i21a-s1.ufhpc>
2020-09-30 19:53:30 -0400

    homework with unused code taken out

--- hw2b_odonnell.py
+++ hw2b_odonnell.py
@@ -5,20 +5,15 @@ from math import pi
 
 # constants 
 G = 6.67*10**-11 # Gravitational Constant - m^3kg^(-1)s^(-2) 
-#print(G, " ")
 M = 5.97*10**24 # Mass of Earth - kg 
-#print(M, " ")
 R = 6371000 # Radius of Earth - km 
-#print(R, " ")
 
 # user input
 t = float(input("Please enter a time interval in minuites: "))
-T = t*60
-#print(T)
+T = t*60 #putting t in seconds
 
 # equation
 h = (((G*M*T**2) / (4*pi**2))**(1/3)) - R
-#h = pow(((G*M*pow(T,2)) / (4*pow(3.14,2))), (1/3)) - R
 
 # output 
 print("The altitude of the sattellite is", h, "meters")
